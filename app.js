393f07a08cea306b0b06875c660786f33055186c
zedd27 <67116788+zedd27@users.noreply.github.com>
2023-03-04 03:40:06 +0530

    Production url replaced

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ passport.use(
         {
             clientID: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/secrets",
+            callbackURL: "https://secrets2703.onrender.com/auth/google/secrets",
         },
         function (accessToken, refreshToken, profile, cb) {
             User.findOrCreate({ googleId: profile.id }, function (err, user) {
